ffdb2f7df9077516a9c4bcad7e3ae70a1b66ed07
Taniya1493 <taniyapushey@gmail.com>
2025-02-02 13:02:00 +0530

    added question-service,quiz-service,service-registry and api-gateway for quiz-app

---
+++ question-service/src/main/java/com/taniya/question_service/model/Response.java
@@ -0,0 +1,31 @@
+package com.taniya.question_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
---
+++ quiz-service/src/main/java/com/taniya/quiz_service/model/QuizDto.java
@@ -0,0 +1,38 @@
+package com.taniya.quiz_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizDto {
+    String categoryName;
+    Integer numQuestions;
+    String title;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getNumQuestions() {
+        return numQuestions;
+    }
+
+    public void setNumQuestions(Integer numQuestions) {
+        this.numQuestions = numQuestions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
